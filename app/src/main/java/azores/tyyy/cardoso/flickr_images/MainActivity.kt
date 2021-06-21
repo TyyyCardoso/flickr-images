2e3282881c773c9a04e088788ddac1543a17bfd7
TyyyCardoso <tiagoouriquecardoso@gmail.com>
2021-06-21 11:18:36 +0100

    Create project skeleton files

---
+++ app/src/main/java/azores/tyyy/cardoso/flickr_images/MainActivity.kt
@@ -0,0 +1,11 @@
+package azores.tyyy.cardoso.flickr_images
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
