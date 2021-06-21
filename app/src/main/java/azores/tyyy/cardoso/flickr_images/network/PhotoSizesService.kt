91e922bb48bc2413804032ff6287dffc4cd43910
tiago0liveira77 <tiago66.oliveira@hotmail.com>
2021-06-21 13:34:25 +0100

    Query feature added on PhotoSizes

--- app/src/main/java/azores/tyyy/cardoso/flickr_images/network/PhotoSizesService.kt
+++ app/src/main/java/azores/tyyy/cardoso/flickr_images/network/PhotoSizesService.kt
@@ -4,8 +4,11 @@ import azores.tyyy.cardoso.flickr_images.constants.Constants
 import azores.tyyy.cardoso.flickr_images.models.PhotoSizesModel
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface PhotoSizesService {
-    @GET("?method=flickr.photos.getSizes&api_key=${Constants.APP_ID}&photo_id=7803551540&format=json&nojsoncallback=1")
-    fun getInfo() : Call<PhotoSizesModel>
+    @GET("?method=flickr.photos.getSizes&api_key=${Constants.APP_ID}&photo_id=51261914979&format=json&nojsoncallback=1")
+    fun getInfo(
+        @Query("photo_id") photo_id : String
+    ) : Call<PhotoSizesModel>
 }
